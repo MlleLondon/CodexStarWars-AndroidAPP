2622d22d09594ffa4c789f3df3f421121b958e67
julien <julien.mrss@gmail.com>
2024-04-12 12:04:27 +0200

    update VaisseauFragment

--- app/src/main/java/com/codexstarwars_androidapp/fragments/VaisseauFragment.java
+++ app/src/main/java/com/codexstarwars_androidapp/fragments/VaisseauFragment.java
@@ -7,60 +7,39 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.codexstarwars_androidapp.R;
+import com.codexstarwars_androidapp.models.Vaisseau;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link VaisseauFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class VaisseauFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public VaisseauFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VaisseauFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VaisseauFragment newInstance(String param1, String param2) {
-        VaisseauFragment fragment = new VaisseauFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ImageView imageViewVaisseau;
+    private TextView textViewVaisseauName, textViewVaisseauDescription, textViewVaisseauLongueur,
+            textViewVaisseauVitesse, textViewVaisseauEquipage, textViewVaisseauMoteur;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_vaisseau, null);
+
+        textViewVaisseauName = v.findViewById(R.id.textViewVaisseauName);
+        textViewVaisseauDescription = v.findViewById(R.id.textViewVaisseauDescription);
+        textViewVaisseauLongueur = v.findViewById(R.id.textViewVaisseauLongueur);
+        textViewVaisseauVitesse = v.findViewById(R.id.textViewVaisseauVitesse);
+        textViewVaisseauEquipage = v.findViewById(R.id.textViewVaisseauEquipage);
+        textViewVaisseauMoteur = v.findViewById(R.id.textViewVaisseauMoteur);
+        imageViewVaisseau = v.findViewById(R.id.imageViewVaisseau);
+
+        return v;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_vaisseau, container, false);
+    public void onSelectVaisseau(Vaisseau vaisseau) {
+
+        textViewVaisseauName.setText(vaisseau.getNom());
+        textViewVaisseauDescription.setText(vaisseau.getDescription());
+        textViewVaisseauLongueur.setText(vaisseau.getLongueur());
+        textViewVaisseauVitesse.setText(vaisseau.getVitesse());
+        textViewVaisseauEquipage.setText(vaisseau.getEquipage());
+        textViewVaisseauMoteur.setText(vaisseau.getMoteur());
     }
 }
